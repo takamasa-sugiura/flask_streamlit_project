eca298bead5fec0627bd6fa70e89f35912ab1c0c
takamasa-sugiura <130st2635842@gmail.com>
2025-03-10 20:06:05 +0900

    Fix credentials.json path

--- backend/spreadsheet.py
+++ backend/spreadsheet.py
@@ -6,7 +6,7 @@ from google.oauth2.service_account import Credentials
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
 # `backend/credentials.json` を使用
-creds = Credentials.from_service_account_file("spreadsheet-to-pdf-f0b419fd4345", scopes=scope)
+creds = Credentials.from_service_account_file("backend/spreadsheet-to-pdf-f0b419fd4345.json", scopes=scope)
 client = gspread.authorize(creds)
 
 def copy_sheet(month):
